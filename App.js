b588a6c2af954bdc07e8947fb1d6db89e6c5392d
Eduarda Brisch Dalmas <eduardadalmas@ienh.com.br>
2020-05-12 20:10:44 -0300

    mergeee

--- App.js
+++ App.js
@@ -1,61 +1,19 @@
-import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.openDrawer()}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.openDrawer()} title="Go back home" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator 
-      initialRouteName="Home" 
-      hideStatusBar 
-      drawerStyle={{
-        backgroundColor: 'pink',
-      }}
-      drawerContentOptions={{
-        activeBackgroundColor: '#fff',
-        activeTintColor: 'pink',
-        inactiveTintColor: '#fff',
-        itemStyle: {
-          marginHorizontal: 20,
-          paddingLeft: 50,
-          height: 50,
-          borderTopRightRadius: 20,
-          borderBottomRightRadius: 20,
-          justifyContent: "center",
-        },
-        labelStyle: {
-          fontSize: 20,
-          fontWeight: "bold",
-        }
-      }}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} options={{
-          drawerIcon: ({ color, focused, size }) => {} 
-        }}/>
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
