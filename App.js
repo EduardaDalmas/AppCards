4d010349032612cc3350bc3c8923b8c3461e8047
Eduarda Brisch Dalmas <eduardadalmas@ienh.com.br>
2020-05-16 12:04:05 -0300

    login

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from "./src/Routes";
+//import Provider from 'react-redux';
+//import { createStore } from 'redux';
+//import reducer from 'reducer';
+
+//const store = createStore(reducer)
 
 function App() {
 
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    //<Provider store={store}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    //</Provider>
   );
 }
 
--- src/auth/screens/signIn.js
+++ src/auth/screens/signIn.js
@@ -3,25 +3,27 @@ import { Button, View, Text, TextInput, Image } from 'react-native';
 import Imag from '../../../assets/check.png';
 
 function SignIn({ navigation }) {
-    const [value, onChangeText] = React.useState('Email');
+    const [email, setEmail] = React.useState('Email');
+    const [senha, setSenha] = React.useState('Senha');
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly', backgroundColor: "#ffffcc" }}>
         <Image 
-        source={Imag} 
-        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-      /> 
-        <TextInput
-            style={{ height: 50, borderColor: 'gray', width: '80%', borderWidth: 1, padding: 10}}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-        />
-        <TextInput secureTextEntry
-            style={{ height: 50, borderColor: 'gray', width: '80%', borderWidth: 1, padding: 10 }}
-            onChangeText={text => onChangeText(text)}
-            value={value}
-        />
-       
-        <Button title="Entrar" onPress={() => navigation.navigate("Home")}/>
+          source={Imag} 
+          style={{ height: 200, width: 200, resizeMode: "contain" }}
+        /> 
+        <View style={{ width: "100%", height: 100 ,justifyContent: 'space-between', alignItems: "center"}}>
+          <TextInput
+              style={{ height: 50, borderColor: 'gray', width: '80%', borderWidth: 1, borderRadius: 8, padding: 10, margin: 10}}
+              onChangeText={text => setEmail(text)}
+              value={email}
+          />
+          <TextInput secureTextEntry
+              style={{ height: 50, borderColor: 'gray', width: '80%', borderWidth: 1, borderRadius: 8, padding: 10 }}
+              onChangeText={text => setSenha(text)}
+              value={senha}
+          />
+       </View>
+        <Button title="Entrar" style={{borderRadius: 8, background: "#ff0000"}} onPress={() => navigation.navigate("Home")}/>
       </View>
     );
   }
